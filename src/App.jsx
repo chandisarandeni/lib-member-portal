abed81eb0a4c0521c319c078eeee2fba9518138a
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-21 01:40:30 +0530

    feat: add LoginPage component ✅
    feat: add LoginPage component with responsive design and password visibility toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
 
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="text-center bg-white p-10 rounded-xl shadow-md">
-          <h1 className="text-3xl font-semibold text-gray-800">
-            Tailwind CSS is working!
-          </h1>
-        </div>
-      </div>
+      <LoginPage />
     </>
   );
 }
